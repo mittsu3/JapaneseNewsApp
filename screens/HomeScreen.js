b28b60317bfc3fd591468a6c538536914e446a19
mittsu3 <52969769+mittsu3@users.noreply.github.com>
2020-03-27 16:02:52 +0900

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  RefreshControl
+} from "react-native";
+import ListItem from "../components/ListItem";
+import Loading from "../components/Loading";
+import Constants from "expo-constants";
+import axios from "axios";
+import { ScrollView } from "react-native-gesture-handler";
+
+export default HomeScreen = props => {
+  const { navigation } = props;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    fetchArticles();
+    setRefreshing(false);
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate("Article", { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+      {loading && <Loading />}
+    </SafeAreaView>
+  );
+};
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  leftContainer: {
+    width: 100
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between"
+  },
+  text: {
+    fontSize: 16
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray"
+  }
+});
