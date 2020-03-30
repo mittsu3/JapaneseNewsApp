116a7950dfaa0ce8fb70c3c72c7c4b7a8bac6f40
mittsu3 <52969769+mittsu3@users.noreply.github.com>
2020-03-30 13:54:00 +0900

    Add files via upload

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -6,9 +6,52 @@ import ArticleScreen from "../screens/ArticleScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ClipScreen from "../screens/ClipScreen";
 import { FontAwesome } from "@expo/vector-icons";
-
+import AnimationScreen from "../screens/AnimationScreen";
+import GameScreen from "../screens/GameScreen";
+import CoronaScreen from "../screens/CoronaScreen";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { SafeAreaView, StyleSheet, ScrollView } from "react-native";
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+const GameStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Game"
+        component={GameScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
+const CoronaStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Corona"
+        component={CoronaScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const AnimeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Anime"
+        component={AnimationScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
 
 const HomeStack = () => {
   return (
@@ -40,6 +83,12 @@ const screenOption = ({ route }) => ({
       iconName = "home";
     } else if (route.name === "Clip") {
       iconName = "bookmark";
+    } else if (route.name === "Anime") {
+      iconName = "tv";
+    } else if (route.name === "Game") {
+      iconName = "gamepad";
+    } else if (route.name === "Corona") {
+      iconName = "certificate";
     }
     return <FontAwesome name={iconName} size={size} color={color} />;
   }
@@ -47,11 +96,22 @@ const screenOption = ({ route }) => ({
 
 export default AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOption}>
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Clip" component={ClipStack} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={screenOption}>
+          <Tab.Screen name="Home" component={HomeStack} />
+          <Tab.Screen name="Anime" component={AnimeStack} />
+          <Tab.Screen name="Game" component={GameStack} />
+          <Tab.Screen name="Corona" component={CoronaStack} />
+          <Tab.Screen name="Clip" component={ClipStack} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
